58b1d28f32b8ed3cad920c055f214c12f61f3062
hoan2222 <141321671+hoan2222@users.noreply.github.com>
2024-03-28 21:11:28 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,10 @@ const nextConfig = {
     basePath: '/medival-memes',
 
     output: 'export',
-   
+
+     images: {
+    unoptimized: true,
+     }
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
    
