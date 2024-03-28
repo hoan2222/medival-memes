40d4d9ac52469086fd1338611e3d88c42a05006a
anelnoraa <165261794+anelnoraa@users.noreply.github.com>
2024-03-28 16:31:08 +0100

    done

--- app/ui/components/box/index.jsx
+++ app/ui/components/box/index.jsx
@@ -6,6 +6,7 @@ export default function Box({title, content}){
         <div className="p-4 bg-white rounded-[35px] sm:w-[30%]">
             <h1 className="text-2xl font-bold">{title}</h1>
             <p className="text-2xl text-wrap">{content}</p>
+            
         </div>
     )
 }
