55fff6c6eab86fa93fb6ee44c815761077fce5bf
hoan2222 <141321671+hoan2222@users.noreply.github.com>
2024-03-28 21:21:18 +0100

    Update index.jsx

--- app/ui/components/landing_screen/mainImage/index.jsx
+++ app/ui/components/landing_screen/mainImage/index.jsx
@@ -5,7 +5,7 @@ export default function MainImage(){
 
     return(
         
-        <div className="flex flex-col justify-between align-center pt-72 pb-20 h-[100vh] bg-cover bg-[url('/Background_Image.jpg')]" id="landing">
+        <div className="flex flex-col justify-between align-center pt-72 pb-20 h-[100vh] bg-cover bg-[url('/medival-memes/Background_Image.jpg')]" id="landing">
            <div className="flex justify-center">
                     <img src="/medival-memes/MemeGenerator.svg" alt="Memegenerator" width={800} />
            </div>
