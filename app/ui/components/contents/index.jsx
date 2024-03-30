f4c72b3b808e050cd5c339f13fe6feda608ee4f3
anelnoraa <165261794+anelnoraa@users.noreply.github.com>
2024-03-30 12:06:17 +0100

    new try

--- app/ui/components/contents/index.jsx
+++ app/ui/components/contents/index.jsx
@@ -5,7 +5,7 @@ import Card from "./card";
 
 const Content =  async () => {
 
-    const contents = await fetch('/app/api');
+    const contents = await fetch('https://hoan2222.github.io/medival-memes/app/api');
 
     return(
         <section className="flex justify-between overflow-x-hidden w-full no-scrollbar" id="generator">
