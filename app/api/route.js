3b1538392dea648c78c674157a4c027706f806a6
anelnoraa <165261794+anelnoraa@users.noreply.github.com>
2024-03-30 11:52:55 +0100

    done

--- app/api/route.js
+++ app/api/route.js
@@ -1,11 +1,10 @@
 
 
-export async function GET(){
-
-    const reqUrl = 'http://kjflaksjdfhkjsdf.medianewsonline.com/wp-json/wp/v2/titles?&acf_format=standard&_fields=id,title,acf';
-    const req = fetch(reqUrl);
-    const data = await req.json();
-
-    return Response.json(data);
-
-}
+export async function GET() {
+    const res = await fetch('http://kjflaksjdfhkjsdf.medianewsonline.com/wp-json/wp/v2/titles?&acf_format=standard&_fields=id,title,acf', {
+      next: { revalidate: 60 }, // Revalidate every 60 seconds
+    })
+    const data = await res.json()
+   
+    return Response.json(data)
+  }
