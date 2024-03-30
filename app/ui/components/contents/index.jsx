70d7a71335f149edaeb42bc0586127c7d807f8e7
anelnoraa <165261794+anelnoraa@users.noreply.github.com>
2024-03-30 21:47:21 +0100

    new fetch try

--- app/ui/components/contents/index.jsx
+++ app/ui/components/contents/index.jsx
@@ -1,19 +1,21 @@
-'use client'
+import useSWR from 'swr'
 
 import Card from "./card";
 
 
 const Content =  async () => {
 
-    const contents = await fetch('https://hoan2222.github.io/medival-memes/app/api');
+    const { data, error, isLoading} = useSWR('/api', fetcher)
 
+    if (error) return <div>failed to load...</div>
+    if (isLoading) return <div>loading...</div>
     return(
         <section className="flex justify-between overflow-x-hidden w-full no-scrollbar" id="generator">
                  <div className="flex w-full overflow-auto [&>div]:flex-shrink-0 px-10 no-scrollbar">
-                    {contents?.map((content) => (
+                    {data?.map((content) => (
                         <Card 
                         key={content.id}
-                        title={content.title.rendered}
+                        title={data.title.rendered}
                         summary={content.acf.summary}
                         image={content.acf.thumbnail}
                         />
