7c2f34e37f814a5aba43fc85507e87f25031ba31
anelnoraa <165261794+anelnoraa@users.noreply.github.com>
2024-03-30 21:40:59 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /**
  * @type {import('next').NextConfig}
  */
-
-
-
 const nextConfig = {
-
-
-
-    output: 'export',
-
-
-  }
-   
-  module.exports = nextConfig
+  output: 'export',
+  /* config options here */
+}
+ 
+module.exports = nextConfig
