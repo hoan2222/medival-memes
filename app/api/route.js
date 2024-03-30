12dd5ac6d1e6d46c8ca51afe7494e0df1f4929ea
anelnoraa <165261794+anelnoraa@users.noreply.github.com>
2024-03-30 08:17:17 +0100

    new try

--- app/api/route.js
+++ app/api/route.js
@@ -4,7 +4,7 @@ export async function GET(){
 
     const reqUrl = 'http://kjflaksjdfhkjsdf.medianewsonline.com/wp-json/wp/v2/titles?&acf_format=standard&_fields=id,title,acf';
     const req = fetch(reqUrl);
-    const data = (await req).json();
+    const data = await req.json();
 
     return Response.json(data);
 
