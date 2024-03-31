0d9f7d2a9ac8e81aa6ecf9c0703877c4b5944398
anelnoraa <165261794+anelnoraa@users.noreply.github.com>
2024-03-31 21:26:40 +0200

    done changes

---
+++ app/admin/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Admin(){
+
+    return(
+
+        <div className="p-96 flex justify-center">
+            <a href="http://kjflaksjdfhkjsdf.medianewsonline.com/admin">
+                <h1 className="px-10 py-4 rounded-full border-2">Zur Admin-Seite</h1>
+            </a>
+        </div>
+    )
+}
